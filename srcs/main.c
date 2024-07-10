6766402479468a9ce174dde39ff9ab145a2d00f3
Nabil080 <nbellila@student.42.fr>
2024-07-10 21:38:24 +0200

    open infile et outfile + is_heredoc bool

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:22:38 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/10 20:50:41 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/10 21:33:45 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv, char **envp)
 	t_data	data;
 
 	check_args(argc, argv);
-	init_data(&data);
+	init_data(argc, argv, &data);
 	data.paths = get_paths(envp);
 	if (!data.paths)
 		exit_error("An allocation failed", &data);
@@ -26,6 +26,8 @@ int	main(int argc, char **argv, char **envp)
 		exit_error("An allocation failed", &data);
 	if (!check_exec(&data))
 		exit_error("A command is unknown", &data);
+	// todo
+	// maxi_piping(data);
 	free_data(&data);
 	exit(EXIT_SUCCESS);
 }
