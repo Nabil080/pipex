fcd64b211088b3d1586b5e49ed89d76fdf1cb21a
Nabil080 <bellilanabil8@gmail.com>
2024-07-10 16:00:27 +0200

    check if infile exists

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:22:59 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/10 14:02:28 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/10 15:57:04 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,19 @@
 
 # include "libft.h"
 
+/*structs*/
+typedef struct s_data {
+	char	**paths;
+}t_data;
+
+/*check*/
+void	check_args(int argc, char **argv);
+
 /*parsing*/
+t_data *init_data(char **envp);
+
 char	**get_paths(char **envp);
+/*exit*/
+void	exit_error(char *str);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:22:38 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/10 13:59:37 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/10 15:53:11 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	char	**paths;
+	t_data	*data;
 
-	paths = get_paths(envp);
-	ft_putarr(paths);
+	check_args(argc, argv);
+	data = init_data(envp);
 }
