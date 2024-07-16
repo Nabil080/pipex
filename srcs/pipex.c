3c7e0ef1fcb2ffd6adcd8d2cb6eefef02feec315
Nabil080 <nbellila@student.42.fr>
2024-07-16 17:38:35 +0200

    squelette de ft_exec

--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 22:40:30 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/15 22:40:51 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/16 17:37:33 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,33 @@
 static void	ft_exec(t_data data, size_t	index)
 {
 	pid_t	pid;
-	
+	int		fd[2];
+
+	//? pipe 
+	//todo: check error
+	pipe(fd);
+	//? fork 
+	//todo: check error
 	pid = fork();
-	if (pid == 0)
-	{
-		dup2(data.in_fd, 0);
-		if (index == data.count -1)
-			dup2(data.out_fd, 1);
-		execve(data.args[index][0], data.args[index], data.env);
-	}
-	else
+	//? parent attends la fin de l'enfant
+	if (pid != 0)
 	{
 		wait(NULL);
-		printf("Parent pid\n");
+		return ;
 	}
+	//? stdin devient fd_in (infile ou sortie ancienne pipe)
+	dup2(data.in_fd, STDIN_FILENO);
+	//? si derniere commande, stdout devient fd_out
+	if (index == data.count - 1)
+		dup2(data.out_fd, STDOUT_FILENO);
+	//? sinon stdout devient l'entree de la nouvelle pipe
+	else
+		dup2(fd[1], STDOUT_FILENO);
+	//? fd_in devient la sortie de la nouvelle pipe
+	close(fd[1]);
+	data.in_fd = fd[0];
+	//? execute la commande
+	execve(data.args[index][0], data.args[index], data.env);
 }
 
 void	maxi_piping(t_data data)
@@ -41,8 +54,9 @@ void	maxi_piping(t_data data)
 	while (data.args[cmd])
 	{
 		printf("\n-----Executing cmd[%zu]: %s-----\n", cmd, data.args[cmd][0]);
+		printf("fd_in : %d, ft_out : %d\n", data.in_fd, data.out_fd);
 		ft_exec(data, cmd);
-		printf("\n-----DONE-----\n");
+		printf("\nDONE\n");
 		cmd++;
 	}
 }
