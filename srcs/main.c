df5b728838e7643e9f1c7e112393a300e94100fc
Nabil080 <bellilanabil8@gmail.com>
2024-07-16 19:52:09 +0200

    multipipe done, manque gestion crash + heredoc

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,12 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:22:38 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/15 22:31:57 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/16 19:41:24 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-static void	test(t_data data)
-{
-	pid_t	pid;
-
-	pid = fork();
-	if (pid == 0)
-		execve(data.args[0][0], &data.args[0][0], data.env);
-	wait(NULL);
-	printf("Parent\n");
-}
 
 int	main(int ac, char **av, char **env)
 {
@@ -37,8 +27,6 @@ int	main(int ac, char **av, char **env)
 		exit_error("An allocation failed", &data);
 	if (!check_exec(&data))
 		exit_error("A command is unknown", &data);
-	if (0)
-		test(data);
 	// todo : piping
 	maxi_piping(data);
 	free_data(&data);
