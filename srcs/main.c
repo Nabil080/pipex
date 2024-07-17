6aa3d1943ff5677467a9bc73c67225a35f0939b1
Nabil080 <nbellila@student.42.fr>
2024-07-17 19:44:15 +0200

    next command does not wait for previous one to be done

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: nbellila <nbellila@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 11:22:38 by nbellila          #+#    #+#             */
-/*   Updated: 2024/07/17 19:08:37 by nbellila         ###   ########.fr       */
+/*   Updated: 2024/07/17 19:41:20 by nbellila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int ac, char **av, char **env)
 	if (!data.args)
 		exit_error("An allocation failed", &data);
 	if (!check_exec(&data))
-		exit_error("A command is unknown", &data);
+		exit_error("command not found", &data);
 	maxi_piping(data);
 	free_data(&data);
 	exit(EXIT_SUCCESS);
